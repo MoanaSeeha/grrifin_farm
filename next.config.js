3af297d981f624bbaac292a171e14f33c9e9fe22
Davincci <kodeaap@users.noreply.github.com>
2022-10-27 12:55:26 +0700

    issue fixed

--- next.config.js
+++ next.config.js
@@ -155,5 +155,11 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 }
